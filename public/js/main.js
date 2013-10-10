2d428ce90d4355ae066db8029cfe32c456dd2bc8
Jamie Chung <me@jamiechung.me>
2013-10-09 21:58:48 -0700

    Setting up the form validation for empty strings

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,10 @@
 $(document).ready(function(){
+  $('#playlist-form').submit(function(){
+    var playlist_url = $('#playlist-url').val();
+    if ( !playlist_url ) {
+      $('#playlist-url').focus();
+    }
 
+    return false;
+  });
 });
